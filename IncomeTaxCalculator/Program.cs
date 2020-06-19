efaec111e6bf96fa3387a7cf6ca76241833984ba
budong drummond <budong21@hotmail.com>
2020-06-19 13:37:51 +0100

    Add project files.

---
+++ IncomeTaxCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IncomeTaxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Welcome to your income prediction machine, it wont be 100% accurate, but, it will be very close :) ***");
+        }
+    }
+    /// <summary>
+    /// Abstract class to be the base class of AnnualPayee, MonthlyPayee, & HourlyPayee classes.
+    /// We will have abstract methods called CalculateGrossAnnualSalary, CalculateNationalInsuranceDeduction & CalculateIncomeTax.
+    /// Followed by normal methods called CalculateNetAnnualSalary and CalculateNetMonthlySalary
+    /// </summary>
+    abstract class Payee
+    {
+        //Properties
+    }
+}
